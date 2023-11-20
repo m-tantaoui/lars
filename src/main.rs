83d581d10398802f4608fed945134c04811f0345
m-tantaoui <mohamedalmahdi.tantaoui@gmail.com>
2023-11-20 12:56:51 +0100

    vectorize timeit

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,16 @@ use std::time::{Duration, SystemTime};
 
 use lars::gemm::naive_gemm;
 
-fn timeit<F: FnMut() -> T, T>(mut f: F) -> Duration {
-    let start = SystemTime::now();
-    f();
-    let end = SystemTime::now();
-    end.duration_since(start).unwrap()
+fn timeit<F: FnMut() -> T, T>(mut f: F, repeat: usize) -> Vec<Duration> {
+    let durations: Vec<Duration> = (0..repeat)
+        .map(|i| {
+            let start = SystemTime::now();
+            f();
+            let end = SystemTime::now();
+            end.duration_since(start).unwrap()
+        })
+        .collect();
+    durations
 }
 
 fn compute_gflops() {
@@ -30,32 +35,22 @@ fn compute_gflops() {
     // computing C:=AB + C
     // naive_gemm(m, n, k, &a, ld_a, &b, ld_b, &mut c, ld_c);
 
-    let flops: f32 = (2 * m * n * k) as f32 * 1_f32.powi(-9); // number of floating points operations
-    let duration = timeit(|| naive_gemm(m, n, k, a, ld_a, b, ld_b, c, ld_c));
-    let seconds = duration.as_secs() as f32;
-    let gflops = flops / seconds;
-    println!("GigaFlops {:.3} ", gflops);
-    println!("duration in micro seconds {}", duration.as_micros())
+    println!("{}", 10_f32.powi(-9));
+
+    let flops: f64 = (2 * m * n * k) as f64 * 10_f64.powi(-9); // number of floating points operations
+    let durations = timeit(|| naive_gemm(m, n, k, a, ld_a, b, ld_b, c, ld_c), 10);
+    let durations_in_seconds: Vec<f64> = durations
+        .iter()
+        .map(|duration| duration.as_secs_f64())
+        .collect();
+    let gflops: Vec<f64> = durations_in_seconds
+        .iter()
+        .map(|seconds| flops / seconds)
+        .collect();
+    println!("GigaFlops {:?} ", gflops);
+    // println!("duration in micro seconds {}", duration.as_micros())
 }
 
 fn main() {
     compute_gflops()
-    // // defining matrices as vectors (column major matrices)
-    // let m = 3; //number of rows of A
-    // let n = 2; // number of columns of B
-    // let k = 3; // number of columns of A and number of rows of B , they must be equal!!!!!
-
-    // let a = vec![1.0, 1.0, -2.0, -2.0, 1.0, 2.0, 2.0, 3.0, 1.0];
-    // let ld_a = 3; // leading dimension of A
-
-    // let b = vec![-2.0, 1.0, -1.0, 1.0, 3.0, 2.0];
-    // let ld_b = 3; // leading dimension of B
-
-    // let mut c = vec![1.0, -1.0, -2.0, 0.0, 2.0, 1.0];
-    // let ld_c = 3; // leading dimension of C
-
-    // // computing C:=AB + C
-    // naive_gemm(m, n, k, &a, ld_a, &b, ld_b, &mut c, ld_c);
-
-    // display(m, n, ld_c, &c);
 }
