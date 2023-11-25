2977f2893238e7a2042e385816886b2f26dbf72d
m-tantaoui <mohamedalmahdi.tantaoui@gmail.com>
2023-11-25 22:02:32 +0100

    kernel gemm using intrinsic functions

--- src/matmul.rs
+++ src/matmul.rs
@@ -8,6 +8,8 @@ use std::cmp::min;
 use std::fmt;
 use std::slice::Iter;
 
+use core::arch::x86_64::*;
+
 #[allow(clippy::too_many_arguments)]
 pub fn naive_gemm(
     m: usize,
@@ -244,6 +246,121 @@ pub fn block_axpy_ger_gemm(
                     ld_c,
                 )
             }
+        }
+    }
+}
+
+fn gemm_4x4_kernel(
+    k: usize,
+    a: &[f64],
+    ld_a: usize,
+    b: &mut [f64],
+    ld_b: usize,
+    c: &mut [f64],
+    ld_c: usize,
+) {
+    // Declare vector registers to hold 4x4 Columns and load them
+    let mut c_0123_0: __m256d;
+    let mut c_0123_1: __m256d;
+    let mut c_0123_2: __m256d;
+    let mut c_0123_3: __m256d;
+    unsafe {
+        c_0123_0 = _mm256_loadu_pd(&c[ele_ij(0, 0, ld_c)]);
+        c_0123_1 = _mm256_loadu_pd(&c[ele_ij(0, 1, ld_c)]);
+        c_0123_2 = _mm256_loadu_pd(&c[ele_ij(0, 2, ld_c)]);
+        c_0123_3 = _mm256_loadu_pd(&c[ele_ij(0, 3, ld_c)]);
+    }
+
+    for p in 0..k {
+        // declare vector register for loading/broadcasting b[p, j]
+        let mut b_pj: __m256d;
+
+        // declare a vector register to hold the current column of a and load it with four elements of that column
+        let a_0123_p: __m256d;
+        unsafe {
+            a_0123_p = _mm256_loadu_pd(&a[ele_ij(0, p, ld_a)]);
+        }
+
+        // Load/broadcast beta( p,0 )
+        unsafe {
+            b_pj = _mm256_broadcast_sd(&b[ele_ij(p, 0, ld_b)]);
+        }
+
+        // update the first column of c with the current element of a times b[p, 0]
+        unsafe { c_0123_0 = _mm256_fmadd_pd(a_0123_p, b_pj, c_0123_0) }
+
+        // Load/broadcast b[p,1]
+        unsafe {
+            b_pj = _mm256_broadcast_sd(&b[ele_ij(p, 1, ld_b)]);
+        }
+
+        // update the second column of C with the current column of A times  b[p,1]
+        unsafe {
+            c_0123_1 = _mm256_fmadd_pd(a_0123_p, b_pj, c_0123_1);
+        }
+
+        // Load/broadcast b[p,2]
+        unsafe {
+            b_pj = _mm256_broadcast_sd(&b[ele_ij(p, 2, ld_b)]);
+        }
+
+        // update the second column of C with the current column of A times  b[p,1]
+        unsafe {
+            c_0123_2 = _mm256_fmadd_pd(a_0123_p, b_pj, c_0123_2);
+        }
+
+        // Load/broadcast b[p,3]
+        unsafe {
+            b_pj = _mm256_broadcast_sd(&b[ele_ij(p, 3, ld_b)]);
+        }
+
+        // update the second column of C with the current column of A times  b[p,1]
+        unsafe {
+            c_0123_3 = _mm256_fmadd_pd(a_0123_p, b_pj, c_0123_3);
+        }
+    }
+
+    //store the updated results
+    unsafe {
+        _mm256_storeu_pd(&mut c[ele_ij(0, 0, ld_c)], c_0123_0);
+        _mm256_storeu_pd(&mut c[ele_ij(0, 1, ld_c)], c_0123_1);
+        _mm256_storeu_pd(&mut c[ele_ij(0, 2, ld_c)], c_0123_2);
+        _mm256_storeu_pd(&mut c[ele_ij(0, 3, ld_c)], c_0123_3);
+    }
+
+    // println!("{:#?}", gamma_0123_0)
+}
+
+#[allow(clippy::too_many_arguments)]
+pub fn gemm(
+    m: usize,
+    n: usize,
+    k: usize,
+    a: &[f64],
+    ld_a: usize,
+    b: &mut [f64],
+    ld_b: usize,
+    c: &mut [f64],
+    ld_c: usize,
+) {
+    let mr = 4;
+    let nr = 4;
+
+    if m % mr != 0 || n % nr != 0 {
+        panic!("m and n must be multiples of `mr` and `nr`, respectively \n");
+    }
+
+    for j in (0..n).step_by(nr) {
+        for i in (0..m).step_by(mr) {
+            gemm_4x4_kernel(
+                k,
+                &a[ele_ij(i, 0, ld_a)..],
+                ld_a,
+                &mut b[ele_ij(0, j, ld_b)..],
+                ld_b,
+                &mut c[ele_ij(i, j, ld_c)..],
+                ld_c,
+            )
         }
     }
 }
@@ -626,6 +743,52 @@ mod tests {
         block_axpy_ger_gemm(m, n, k, &a, ld_a, &mut b, ld_b, &mut c, ld_c);
 
         naive_gemm(m, n, k, &a, ld_a, &b, ld_b, &mut c_ref, ld_c);
+
+        assert_eq!(c, c_ref);
+    }
+
+    #[bench]
+    fn benchmark_block_kernel_gemm(bencher: &mut Bencher) {
+        // defining matrices as vectors (column major matrices)
+        let m = 4 * 4; //number of rows of A
+        let n = 4 * 4; // number of columns of B
+        let k = 4 * 4; // number of columns of A and number of rows of B , they must be equal!!!!!
+
+        let a: Vec<f64> = (1..=(m * k)).map(|a| a as f64).collect();
+        let ld_a = 4 * 4; // leading dimension of A (number of rows)
+
+        let mut b: Vec<f64> = (1..=(k * n)).map(|a| a as f64).collect();
+        let ld_b = 4 * 4; // leading dimension of B (number of rows)
+
+        let mut c: Vec<f64> = (1..=(m * n)).map(|a| a as f64).collect();
+        let ld_c = 4 * 4; // leading dimension of C (number of rows)
+
+        bencher.iter(|| {
+            gemm(m, n, k, &a, ld_a, &mut b, ld_b, &mut c, ld_c);
+        });
+    }
+
+    #[test]
+    fn test_block_kernel_gemm() {
+        // defining matrices as vectors (column major matrices)
+        let m = 4 * 4; //number of rows of A
+        let n = 4 * 4; // number of columns of B
+        let k = 4 * 4; // number of columns of A and number of rows of B , they must be equal!!!!!
+
+        let a: Vec<f64> = (1..=(m * k)).map(|a| a as f64).collect();
+        let ld_a = 4 * 4; // leading dimension of A (number of rows)
+
+        let mut b: Vec<f64> = (1..=(k * n)).map(|a| a as f64).collect();
+        let ld_b = 4 * 4; // leading dimension of B (number of rows)
+
+        let mut c: Vec<f64> = (1..=(m * n)).map(|a| a as f64).collect();
+        let mut c_ref: Vec<f64> = (1..=(m * n)).map(|a| a as f64).collect();
+        let ld_c = 4 * 4; // leading dimension of C (number of rows)
+
+        // computing C:=AB + C
+        naive_gemm(m, n, k, &a, ld_a, &mut b, ld_b, &mut c, ld_c);
+
+        gemm(m, n, k, &a, ld_a, &mut b, ld_b, &mut c_ref, ld_c);
 
         assert_eq!(c, c_ref);
     }
